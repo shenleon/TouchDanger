f0ee91c2fb44d8f5176511fc745ae510fd0b420e
shenhj <hj_shen@163.com>
2015-10-09 19:44:08 +0800

    地图

--- Classes/layer/MainSpriteLayer.cpp
+++ Classes/layer/MainSpriteLayer.cpp
@@ -8,15 +8,35 @@ bool MainSpriteLayer::init()
 		return false;
 	origin = CCDirector::sharedDirector()->getVisibleOrigin();
 	visibleSize = CCDirector::sharedDirector()->getVisibleSize();
+	//添加背景底图
+	CCTMXTiledMap * map = CCTMXTiledMap::create("map/road.tmx");
+	this->addChild(map, 1, 1);	
+	//map->setAnchorPoint(ccp(0.5f, 0.5f));
+	//map->setPosition(ccp(visibleSize.width / 2, visibleSize.height / 2));
+
+	//CCLayerColor * bg = CCLayerColor::create(ccc4(0, 255, 0, 100));
+	//bg->setContentSize(visibleSize);
+	////bg->setAnchorPoint(ccp(0.5f, 0.5f));
+	//bg->setPosition(ccp(0,0));
+	//this->addChild(bg);
+
+	//初始化npc列表
+	npcs = new CCArray();
+	npcs->autorelease();
+	npcs->retain();
 	//添加角色
 	playerSprite = RoleSprite::create();
-	playerSprite->setPosition(ccp(visibleSize.width/2, playerSprite->getContentSize().height / 2+30));
+	playerSprite->setPosition(ccp(visibleSize.width*0.4, playerSprite->getContentSize().height / 2+30));
 	this->addChild(playerSprite);
-	//初始化npc列表
-	npcs = CCArray::create();
 	//启动npc添加事件
-	this->schedule(schedule_selector(MainSpriteLayer::updateNpc), 3, 10, 0);
+	this->schedule(schedule_selector(MainSpriteLayer::updateNpc), 1);
 	return true;
+}
+
+void MainSpriteLayer::onExit()
+{
+	npcs->removeAllObjects();
+	npcs = NULL;
 }
 
 void MainSpriteLayer::singleTouchDirecting(CCPoint point)
@@ -27,21 +47,46 @@ void MainSpriteLayer::singleTouchDirecting(CCPoint point)
 void MainSpriteLayer::singleTouchEndsIn(CCPoint point)
 {
 	this->getMainResourceDelegate()->incrLife(100);
+	playerSprite->stop();
 }
 
 void MainSpriteLayer::addNpc()
 {
 	NpcSprite * npc = NpcSprite::create();
+	int tag = time(NULL);
+	npc->setName(CCString::createWithFormat("npc%d",tag));
 	npc->setPosition(ccp(visibleSize.width - npc->getContentSize().width / 2, npc->getContentSize().height / 2 + 30));
-	this->addChild(npc, 1, time(NULL));
+	this->addChild(npc, 1, tag);
 	npcs->addObject(npc);
-
-	CCAction * action = CCMoveTo::create(5.0f, ccp(visibleSize.width / 2, npc->getPositionY()));
+	CCAction * action = CCMoveTo::create(10.0f, ccp(0, npc->getPositionY()));
 	npc->runAction(action);
+}
 
+void MainSpriteLayer::removeNpc(NpcSprite * npc)
+{
+	this->removeChild(npc);
+	npcs->removeObject(npc);
+	npc = NULL;
 }
 
 void MainSpriteLayer::updateNpc(float ft)
 {
-	this->addNpc();
+	int n = npcs->count();
+	if (n < NPC_MAX)
+	{
+		this->addNpc();
+	}
+	CCObject * obj = NULL;
+	NpcSprite * npc = NULL;
+	CCARRAY_FOREACH(npcs, obj)
+	{
+		if (obj == NULL) continue;
+		npc = dynamic_cast<NpcSprite*>(obj);
+		if (npc == NULL) continue;
+		CCLOG("x = %d,y=%d,name=%s", npc->getPositionX(),npc->getPositionY(),npc->getName()->getCString());
+		if (npc->getPositionX() <= 0)
+		{
+			this->removeNpc(npc);
+		}
+	}
 }
--- Classes/layer/MainSpriteLayer.h
+++ Classes/layer/MainSpriteLayer.h
@@ -6,6 +6,8 @@
 #include "sprite\RoleSprite.h"
 #include "sprite\NpcSprite.h"
 #include "MainResourceDelegate.h"
+/*场景中同时出现精灵的个数*/
+#define NPC_MAX 1
 
 /*精灵显示层*/
 class MainSpriteLayer : public cocos2d::CCLayer , public MainTouchDelegate
@@ -20,12 +22,15 @@ public:
 	CC_SYNTHESIZE(MainResourceDelegate *, p_mMainResourceDelegate, MainResourceDelegate);
 public:
 	virtual bool init();
+	virtual void onExit();
 	/*触摸响应*/
 	virtual void singleTouchDirecting(cocos2d::CCPoint point);
 	/*触摸响应结束*/
 	virtual void singleTouchEndsIn(cocos2d::CCPoint point);
 	/*添加一个npc*/
 	void addNpc();
+	/*移除一个npc*/
+	void removeNpc(NpcSprite * npc);
 	/*更新npc*/
 	void updateNpc(float ft);
 	CREATE_FUNC(MainSpriteLayer);
